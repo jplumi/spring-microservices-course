7b73e6e5e20a6ff0e140b791d15b94ad965efdc8
jplumi <59978477+jplumi@users.noreply.github.com>
2022-05-03 15:08:58 -0300

    add eureka-server module, eureka communication

--- customer/src/main/java/com/microservices/customer/CustomerService.java
+++ customer/src/main/java/com/microservices/customer/CustomerService.java
@@ -21,7 +21,7 @@ public class CustomerService {
         repository.saveAndFlush(customer);
 
         FraudCheckResponse response = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
